8f67076b594538fa155d7c4150a9be9de249a919
Simon Gorunovich <blaineighteen@gmail.com>
2025-01-31 02:27:39 +0800

    prettier added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
+import { prisma } from "@/shared/lib/db";
 import { Button } from "@/shared/ui/button";
+import { Card, CardTitle } from "@/shared/ui/card";
 
-export default function Home() {
+export default async function Home() {
+  const games = await prisma.game.findMany();
+  console.log(games);
   return (
     <div className="">
       <Button size="lg">Hello</Button>
+      {games.map((game) => {
+        return (
+          <Card key={game.id}>
+            <CardTitle>{game.name}</CardTitle>
+          </Card>
+        );
+      })}
     </div>
   );
 }
